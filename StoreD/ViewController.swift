899b659a0961bfc5d931d0cd91e471e52bc5f259
Freeman136 <a13661777@gmail.com>
2024-02-26 17:25:26 +0300

    first commit

---
+++ StoreD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoreD
+//
+//  Created by Andrew on 26.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
