be506313db6de3f65ecc465ab9ffe6aa681bfb16
Freeman136 <a13661777@gmail.com>
2024-02-27 12:43:32 +0300

    create UIPageViewController

---
+++ StoreD/PageDatailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  StoreD
+//
+//  Created by Andrew on 26.02.2024.
+//
+
+import UIKit
+
+class PageDatailViewController: UIViewController {
+    var index = 0
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        
+    }
+
+}
+
+extension PageDatailViewController {
+    func getInstance(index: Int) ->PageDatailViewController {
+        
+    }
+}
